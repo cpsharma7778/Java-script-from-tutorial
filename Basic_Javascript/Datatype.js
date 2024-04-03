5f22b5c71464e0e04cc3a994277b0e49d8dd8043
cpsharma7778 <126846806+cpsharma7778@users.noreply.github.com>
2024-04-03 05:53:46 +0000

    Datatype

---
+++ Basic_Javascript/Datatype.js
@@ -0,0 +1,11 @@
+let Name="Cp";
+let Last_Name="Sharma";
+const Number=1234567890;
+
+let City;
+let Dream=null;
+let Future_Brigt=true;
+
+// console.log(typeof Name)
+// console.table([typeof Name,typeof Last_Name, typeof Number, typeof City, typeof Dream, typeof Future_Brigt]) result show is diffrent diffrent datatype
+// console.table(typeof[Name,Last_Name,Number,City,Dream,Future_Brigt])  result is object
