ff2953d25d732c28ac8a941b8540d426ad23fb77
cpsharma7778 <126846806+cpsharma7778@users.noreply.github.com>
2024-04-04 07:37:19 +0000

    String

---
+++ Basic_Javascript/String.js
@@ -0,0 +1,47 @@
+// let name="cp sharma";
+
+
+//  console.log(name.length);
+//  console.log(name);
+//   console.log(name.indexOf("c"));
+
+// Number
+// const number=123.2345;
+//   console.log(number.toPrecision(5));
+
+//   Maths
+// console.log(Math);
+// console.log(Math.random());
+    
+
+// let number=[12,11,10,1,2,100]
+// console.log(number);
+// // ath.number.Max();
+// // console.log(number.Max);
+//  console.log(number.length);
+
+
+// Date
+
+// let time=new Date()
+// console.log(time.toLocaleString());
+// console.log(time.toString());
+// console.log(time.toTimeString());
+// console.log(time.getHours());
+// console.log(time.toTimeString())
+
+// Array
+
+// Splice
+// let arr1=[1,2,3,4,5,6,7]
+// let arr2=arr1.splice(1,3)
+// console.log(arr1);
+// console.log(arr2);
+
+// slice
+
+// let arr1 =[1,2,3,4,5,6,7]
+// let arr2=arr1.slice(0,2)
+// console.log(arr1);
+// console.log(arr2);
+
