61fd3ac171a8310c46e8307ff392ef0e34eb3fc9
cpsharma7778 <126846806+cpsharma7778@users.noreply.github.com>
2024-04-08 11:49:30 +0000

    Loops

---
+++ Basic_Javascript/loops.js
@@ -0,0 +1,24 @@
+// for (let i =1; i<=10; i++) {
+//     console.log(i*2);
+    
+// }
+
+
+//  for (let i = 1; i <=10; i++) {
+//       console.log(i);
+//       for (let j = 1; j <=10; j++) {
+//         //  console.log(i,j);
+//       console.log(i + "*" +j +"="+i*j);
+        
+//       }
+    
+//  }
+//  Break And Continue
+for (let index = 1; index <=15; index++){
+  if(index==7){
+    console.log("7 is last");
+    break;
+  }
+  console.log(index);
+  
+}
