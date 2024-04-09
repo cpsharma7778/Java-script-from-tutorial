2410634e639423131eefbfa687280a9b0ec91a0a
cpsharma7778 <126846806+cpsharma7778@users.noreply.github.com>
2024-04-09 09:53:22 +0000

    DoWhile Loops

---
+++ Basic_Javascript/dowhile.js
@@ -0,0 +1,18 @@
+//  let i=1;
+//  while (i<=10) {
+    
+//     console.log(i*2);
+//     i++
+//  }
+
+//  for (let i = 1; i <=10; i++) {
+
+// console.log(i*2);
+    
+//  }
+
+let i=1
+do {
+    console.log(i*2);
+    i++
+} while (i<=10);
