792f42ab5cfd69f7f0a65eb9f462a80fc65ad729
cpsharma7778 <126846806+cpsharma7778@users.noreply.github.com>
2024-04-12 07:29:07 +0000

    forof,forin,foreach

---
+++ Basic_Javascript/forofandforeach.js
@@ -0,0 +1,25 @@
+// let arr=[1,"cp",2,"dp",3,4,5];
+
+// for (const num of arr) {
+//     console.log(num)
+// }
+
+
+
+// forin
+// for in loop its not work in map method
+
+// let myobj={
+//     vk:"virat kohli",
+//     cp:"cp sharma",
+//     js:"java script"
+// }
+// for (const resu in myobj) {
+//  console.log(resu);  
+// }
+
+let Playername=["virat_kohli","rohit_sharma","jasprit_bumrah","Md_amir"]
+
+Playername.forEach((name)=>{
+   console.log(name);
+})
